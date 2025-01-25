0381b1258077b608908f8881c0a58b471090ae5e
RoadToRight <syedsameershah0786@gmail.com>
2025-01-25 10:09:03 -0800

    Update vite.config.js

--- SwirlFrontend/vite.config.js
+++ SwirlFrontend/vite.config.js
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import dotenv from "dotenv"
-dotenv.config({path:".env"})
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
+dotenv.config({ path: '.env' });
 
 // https://vite.dev/config/
 export default defineConfig({
-  server:{
-    port: parseInt(process.env.VITE_PORT)
+  server: {
+    port: parseInt(process.env.VITE_PORT),
   },
   plugins: [react()],
   build: {
@@ -14,4 +14,7 @@ export default defineConfig({
       external: ['gsap'], // Externalize GSAP if it's not bundled
     },
   },
-})
+  optimizeDeps: {
+    include: ['gsap', 'gsap/ScrollTrigger'], // Ensure GSAP and ScrollTrigger are included in dev dependencies
+  },
+});
