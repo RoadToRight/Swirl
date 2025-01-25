7f3c5efb5cbffa3ad47c11e0fc0f9e73b1ea40d6
RoadToRight <syedsameershah0786@gmail.com>
2025-01-25 13:54:27 -0800

    Update Quotes.jsx

--- SwirlFrontend/src/Components/Quotes.jsx
+++ SwirlFrontend/src/Components/Quotes.jsx
@@ -12,7 +12,22 @@ const Quotes = () => {
   const {DarkLight ,Testinomials } = useContext(Context1)
 
 
-
+  useEffect(() => {
+    // GSAP animation using the global `gsap` from window object
+    window.gsap.from('.Quote', {
+      x: -300,  // Start from 200px below the element's initial position
+      opacity: 0,  // Start from 0 opacity
+      duration: 1,  // Duration for the animation
+      ease: 'power3.out',  // Easing function to smooth the transition
+      scrollTrigger: {
+        trigger: '.Quote',  // The element to trigger the animation
+        start: '18% 80%',  // Start animation when the top of the element reaches 80% from the top of the viewport
+        end: 'bottom 30%',    // End when the top of the element reaches 30% from the top of the viewport
+        scrub: 1,  // Smoothly scrub the animation based on scroll position
+        // markers: true,  // Show scroll trigger markers for debugging (you can remove it once you're done debugging)
+      },
+    });
+  }, []);
 
 
   let Textcolor = DarkLight ? "black" : "white" 
