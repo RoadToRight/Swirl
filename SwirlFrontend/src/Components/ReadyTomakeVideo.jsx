8ac1507e16bb15f45cc5a5f0902994d5ff41c891
RoadToRight <syedsameershah0786@gmail.com>
2025-01-25 13:56:41 -0800

    Update ReadyTomakeVideo.jsx

--- SwirlFrontend/src/Components/ReadyTomakeVideo.jsx
+++ SwirlFrontend/src/Components/ReadyTomakeVideo.jsx
@@ -20,6 +20,22 @@ const ReadyTomakeVideo = () => {
 
 
 
+  useEffect(() => {
+    // GSAP animation using the global `gsap` from the window object
+   window.gsap.from(ready.current, {
+      y: 400,
+      opacity:0,        // Start from 200px below the element's initial position
+      duration: 3,     // Duration for the animation
+      ease: "power3.out", // Easing function to smooth the transition
+      scrollTrigger: {
+        trigger:readyparent.current,    // The element to trigger the animation
+        start: "top 80%",      // Start animation when the top of the element reaches 80% from the top of the viewport
+        end: "bottom 80%",        // End when it reaches 30% from the top of the viewport
+        scrub: 3,           // Smoothly scrub the animation based on scroll position
+        // markers: true,         // Show scroll trigger markers for debugging
+      }
+    });
+  }, []);
 
 
   const scrollToTop = () => {
