fd69cf609388caf07a644c58a2459b53a41611be
RoadToRight <syedsameershah0786@gmail.com>
2025-01-25 14:03:01 -0800

    Update Circle.jsx

--- SwirlFrontend/src/Components/Circle.jsx
+++ SwirlFrontend/src/Components/Circle.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState,useEffect } from "react";
 import styled from "styled-components";
 import Button from "./Button";
 import Context1 from "../Context/Context1";
