9f8ff7bdbb186387c7002018783ead369cb69210
RoadToRight <syedsameershah0786@gmail.com>
2025-01-25 10:56:06 -0800

    Update TopBrandsWithCircle.jsx

--- SwirlFrontend/src/Components/TopBrandsWithCircle.jsx
+++ SwirlFrontend/src/Components/TopBrandsWithCircle.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useEffect, useRef, useState } from "react";
 import Context1 from "../Context/Context1";
 import styled from "styled-components";
 // import gsap from "gsap";
-// import { useGSAP } from "@gsap/react";
+import { useGSAP } from "@gsap/react";
 
 const TopBrandsWithCircle = () => {
   const { DarkLight } = useContext(Context1);
